95723f0d7e58c3838c566bf74e7d49f5558bd60d
Daniel Simão <simaodeveloper@gmail.com>
2020-02-13 19:25:17 -0300

    chore: created bootcamps endpoints

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ const {
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).send({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).send({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(201).send({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).send({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).send({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 app.listen(
   PORT,
   () => console.log(`Server running in ${NODE_ENV} mode on  ${DOMAIN}:${PORT}`)
